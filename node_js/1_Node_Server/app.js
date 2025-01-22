981970e4b6a2a667b8d345bfdcb162521a216abc
rashq <rashq@rashq.rashq>
2025-01-22 17:06:03 +0530

    solving error

--- node_js/1_Node_Server/app.js
+++ node_js/1_Node_Server/app.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const http = require('http');
 
 const server = http.createServer((req,res)=>{
@@ -8,14 +7,3 @@ const PORT = 3000;
 server.listen(PORT,()=>{
   console.log(`Server running on address http://localhost:${PORT}`)
 });
-=======
-const http = require('http');
-
-const server = http.createServer((req,res)=>{
-  console.log("Someone Clicked the link: ");
-})
-const PORT = 3000;
-server.listen(PORT,()=>{
-  console.log(`Server running on address http://localhost:${PORT}`)
-});
->>>>>>> 5862931 (a)
