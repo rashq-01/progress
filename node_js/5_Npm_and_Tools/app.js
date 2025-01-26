4716624a884a937449c1646da5b7729c233cba47
Rajesh Pandit <rashq122@gmail.com>
2025-01-26 16:30:54 +0530

    updating5

--- node_js/5_Npm_and_Tools/app.js
+++ node_js/5_Npm_and_Tools/app.js
@@ -3,7 +3,7 @@ const http = require("http");
 const server = http.createServer((req,res)=>{
   console.log(req);
 });
-const PORT = 3000;
+const PORT = 2000;
 server.listen(PORT,()=>{
   console.log("Server stated on http://localhost:3000");
 })
