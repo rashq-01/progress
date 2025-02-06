2d51d5e6030c00fa062b927b992e729b86bf791c
Rajesh Pandit <rashq122@gmail.com>
2025-02-06 21:51:45 +0530

    adding ejs and installing ejs

---
+++ node_js/10_Dynamic_UI/routes/userRouter.js
@@ -0,0 +1,19 @@
+//Core module
+const path = require('path');
+
+//External module
+const express = require('express');
+
+//Local Module
+const rootDir = require('../utils/pathUtils.js');
+const { registeredHomes } = require('./hostRouter.js');
+
+const userRouter = express.Router();
+
+
+userRouter.get('/',(req,res,next)=>{
+    console.log(registeredHomes);
+    res.sendFile(path.join(rootDir, 'views', 'home.html'))
+});
+
+module.exports = userRouter;
