187c089c585479f50c7e53bcf31f0626aa2939fe
Rajesh Pandit <rashq122@gmail.com>
2025-02-09 18:43:38 +0530

    operations in dll

--- c-c++/Linked_List/Doubly_Linked_List/1_basic.cpp
+++ c-c++/Linked_List/Doubly_Linked_List/1_basic.cpp
@@ -14,47 +14,7 @@ class Node{
         }
 };
 
-class list{
-    public:
-        Node* head;
-        Node* tail;
-
-        list(){
-            head = NULL;
-            tail = NULL;
-        }
-
-        void push_front(int value){
-            Node* newNode = new Node(value);
-            if(!head){
-                head = tail = newNode;
-            }
-            else{
-                newNode->next = head;
-                head = newNode;
-                newNode->next->prev = head;
-            }
-        }
-
-
-        void printList(){
-            Node* temp = head;
-            int a = 1;
-            cout<<endl<<endl;
-            while(temp != NULL){
-                cout<<a<<"  Data: "<<temp->val<<" prevAdd: "<<temp->prev<<"  presentAddress: "<<temp<<"  nextAddress: "<<temp->next<<endl;
-                temp = temp->next;
-                a++;
-            }
-        }
-};
-
 int main(){
-    list l;
-    l.push_front(5);
-    l.push_front(6);
-    l.push_front(8);
-    l.push_front(3);
-    l.printList();
+    Node* head = new Node(5);
 
 }
