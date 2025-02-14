43f5e178953682a1995245808aabc9f9f9449c4a
Rajesh Pandit <rashq122@gmail.com>
2025-02-14 23:19:17 +0530

    updating

---
+++ node_js/12_Dynamic_Path/controllers/storeController.js
@@ -0,0 +1,57 @@
+const Home = require("../models/home");
+
+exports.getIndex = (req, res, next) => {
+  Home.fetchAll((registeredHomes) =>
+    res.render("store/index", {
+      registeredHomes: registeredHomes,
+      pageTitle: "airbnb Home",
+      currentPage: "index",
+    })
+  );
+};
+
+exports.getHomes = (req, res, next) => {
+  Home.fetchAll((registeredHomes) =>
+    res.render("store/home-list", {
+      registeredHomes: registeredHomes,
+      pageTitle: "Homes List",
+      currentPage: "Home",
+    })
+  );
+};
+
+exports.getBookings = (req, res, next) => {
+  res.render("store/bookings", {
+    pageTitle: "My Bookings",
+    currentPage: "bookings",
+  })
+};
+
+exports.getFavouriteList = (req, res, next) => {
+  Home.fetchAll((registeredHomes) =>
+    res.render("store/favourite-list", {
+      registeredHomes: registeredHomes,
+      pageTitle: "My Favourites",
+      currentPage: "favourites",
+    })
+  );
+};
+
+exports.getHomeDetails = (req,res,next)=>{
+  const homeId = req.params.homeId;
+  console.log('At home detail',homeId);
+  Home.findById(homeId,(home)=>{
+    if(!home){
+      console.log("Homes not found");
+      res.redirect('/homes');
+    }
+    else{
+      console.log('Home details Found',home);
+      res.render('store/home-detail',{
+        pageTitle: "Home Detail",
+        currentPage: "Home",
+        home: home,
+      });
+    };
+  });
+};
