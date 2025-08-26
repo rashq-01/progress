d22a71174c61df4476f92a19de49254db406febd
rashq <rashq122@gmail.com>
2025-08-26 22:21:11 +0530

    Adding Profile Card task

--- React/Lesson-5/src/App.jsx
+++ React/Lesson-5/src/App.jsx
@@ -1,6 +1,7 @@
 import InlineCSS from "./InlineCSS";
 import Button from "./button";
 import './index.css'
+import ProfileCard from "./ProfileCard";
 
 function App() {
   const headingStyle = {color:"red", fontSize:"24px"};
@@ -14,6 +15,7 @@ function App() {
       <br />
       <Button text="Button"/>
       <button className="bg-green-500 text-white px-4 py-2 rounded-lg hover:bg-green-700">Rajesh</button>
+      <ProfileCard Name="Rajesh" Branch="CSE" Age={21}/>
     </>
   )
 }
