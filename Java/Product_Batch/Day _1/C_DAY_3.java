a98329836924f9d40ed8cea693919a406bf24db8
Rajesh <rashq122@gmail.com>
2026-03-25 11:47:44 +0530

    Adding java batch files

---
+++ Java/Product_Batch/Day _1/C_DAY_3.java
@@ -0,0 +1,25 @@
+public class C_DAY_3 {
+    
+}
+
+
+
+
+/*
+
+P1. Print largest digit in a number;
+P2. Happy Number
+p3. Strong Number sum of factorials is equl to the original number
+p4. Automorphic number
+p5. Armstrong number sum of cube of every digit should be equal to number
+
+p6. Ugly Number
+
+
+
+
+
+
+
+
+*/
