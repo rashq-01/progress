5e6b417f841711c4e344896587875fa84a6c37ce
Rajesh <rashq122@gmail.com>
2026-08-03 20:41:25 +0530

    Adding dijkstra

---
+++ c-c++/Graphs/15_Dijkstra_using_SET.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+class Solution {
+  public:
+    vector<int> dijkstra(int V, vector<vector<int>> &edges, int src) {
+        unordered_map<int,vector<pair<int,int>>> adj;
+        for(auto& edge : edges){
+            adj[edge[0]].push_back({edge[1],edge[2]});
+            adj[edge[1]].push_back({edge[0],edge[2]});
+        }
+        
+        set<pair<int,int>> s;
+        vector<int> dest(V,INT_MAX);
+        
+        s.insert({0,src});
+        
+        dest[src] = 0;
+        
+        while(!s.empty()){
+            auto topNode = *s.begin();
+            int currDist = topNode.first;
+            
+            s.erase(s.begin());
+            
+            for(auto& nbr : adj[topNode.second]){
+                auto v = nbr.first;
+                int wt = nbr.second;
+                
+                if(currDist+wt < dest[v]){
+                    
+                    s.erase({dest[v],v});
+                    
+                    dest[v] = currDist+wt;
+                    s.insert({currDist+wt,v});
+                }
+            }
+        }
+        
+        return dest;
+    }
+};
+
+
+int main(){
+
+
+    return 0;
+}
